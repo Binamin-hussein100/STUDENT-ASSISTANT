84e36e224b71b263bb15b25efa9679e6dc55f7d4
C0d3president <kadimasam@gmail.com>
2021-05-06 00:24:59 +0300

    Add access class function to groups.js

---
+++ groups.js
@@ -0,0 +1,19 @@
+
+function accessClass(){
+
+    event.preventDefault();
+
+    let p = document.getElementById("textContainer");
+
+    if (p.style.display === "none") {
+        p.style.display = "block";
+      } 
+      
+    else {
+        p.style.display = "none";
+      }
+    
+      
+  
+    
+}
