2f028836839c8562dfb0747a2ee48583d6b97de1
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-05-05 20:29:48 +0300

    made changes to resource.js file

---
+++ resource.js
@@ -0,0 +1,57 @@
+var a;
+function show_hide(){
+    if(a==1)
+    {
+        document.getElementById("bin").style.display="inline"
+        return a=0
+    }
+    else
+        {
+            document.getElementById("bin").style.display="none"
+            return a=1
+        }
+}
+
+var a;
+function click_hide(){
+    if(a==1)
+    {
+        document.getElementById("more").style.display="inline"
+        return b=0
+    }
+    else
+        {
+            document.getElementById("more").style.display="none"
+            return a=2
+        }
+}
+
+
+
+var a;
+function show_hide(){
+    if(a==1)
+    {
+        document.getElementById("bin").style.display="inline"
+        return a=0
+    }
+    else
+        {
+            document.getElementById("bin").style.display="none"
+            return a=1
+        }
+}
+
+// var a;
+// function show_hide(){
+//     if(a==1)
+//     {
+//         document.getElementById("bin").style.display="inline"
+//         return a=0
+//     }
+//     else
+//         {
+//             document.getElementById("bin").style.display="none"
+//             return a=1
+//         }
+// }
